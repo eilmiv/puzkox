1f332cfbe65b7c4ddcbb80489c5cacfa54b82583
Martin Voigt <MartinVoigtdd@web.de>
2018-08-27 22:47:46 +0200

    gameobject management, collision detection

--- server/GameObject.py
+++ server/GameObject.py
@@ -5,6 +5,9 @@ class GameObject:
     def __init__(self, position, description=""):
         self._position = position
         self.description = description
+        self.exist = True
+        self.updating = True
+        self.current_chunk = None
 
     @property
     def position(self):
@@ -27,7 +30,7 @@ class GameObject:
         else:
             return
 
-    def colliding(self, other):
+    def no_partition_edge(self, other):
         for support_vector, normal_vector in self.edges():
             dividing = True
             for vertex in other.vertices():
@@ -38,6 +41,14 @@ class GameObject:
                 return False
         return True
 
+    def colliding(self, other):
+        return self.no_partition_edge(other) and other.no_partition_edge(self)
+
+    def move(self, delta_t):
+        pass
+
+    def update(self, colliding):
+        self.updating = False
 
 #position
 #hitbox
